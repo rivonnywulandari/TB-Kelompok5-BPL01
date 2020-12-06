67e1adba51c4801e8105cee3fd8a3d94f02fe89f
Rivonny Wulandari <rivonnywulandari19@gmail.com>
2020-12-06 23:03:26 +0700

    First commit

--- src/tbkelompok5/Program.java
+++ src/tbkelompok5/Program.java
@@ -1,10 +1,11 @@
+package tbkelompok5;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//ini adalah driver class
+		//ini driver class
 
 	}
 
