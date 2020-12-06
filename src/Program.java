b685a6e1f7d32a30a9fbe2a228bffb877d868cc8
Rivonny Wulandari <60612466+rivonnywulandari@users.noreply.github.com>
2020-12-06 22:53:24 +0700

    first commit

---
+++ src/Program.java
@@ -0,0 +1,11 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//ini adalah driver class
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module tbkelompok5 {
+}
